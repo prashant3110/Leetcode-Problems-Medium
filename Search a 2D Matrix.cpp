ef5564d7859ed1f5fb14963cf056744dbdb7efc5
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-17 11:54:12 +0530

    Update Search a 2D Matrix.cpp

--- Search a 2D Matrix.cpp
+++ Search a 2D Matrix.cpp
@@ -1,1 +1,26 @@
+/* Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
 
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row. */
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+    
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(matrix[i][j]==target)
+                {
+                    return true;
+                }
+               
+            }
+        }
+    return false;
+        
+    }
+};
