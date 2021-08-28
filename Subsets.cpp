8c52d198be601be9b0fe37b1b65f24ab443dfe64
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-08-28 10:57:47 +0530

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>array;
+        for(int i=0;i<(1<<n);i++)
+        {
+            vector<int>ans;
+            for(int j=0;j<n;j++)
+            {
+                if(i & (1<<j))
+                {
+                 ans.push_back(nums[j]);   
+                }
+            }
+            array.push_back(ans);
+        }
+        
+        return array;
+    }
+};
